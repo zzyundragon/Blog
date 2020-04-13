986e4f85d34930395ea59adf307b4b9c6c10c4e7
yundragon <1210265652@qq.com>
2020-04-13 18:14:18 +0800

    prototype

--- JavaScript/JavaScript设计模式/demo/src/index.js
+++ JavaScript/JavaScript设计模式/demo/src/index.js
@@ -1,10 +1,29 @@
 class Person {
-    constructor(name){
+    constructor(name, age) {
         this.name = name
+        this.age = age
     }
-    getName() {
-        return this.name
+    eat() {
+        alert(`${this.name} eat something`)
+    }
+    speak() {
+        alert(`my name is ${this.name}, age ${this.age}`)
     }
 }
-let p = new Person('jerry')
-alert(p.getName())
+class NewPerson extends Person {
+    constructor(...args) {
+        super(...args)
+        this.money = money
+    }
+    getInfo() {
+        alert(`my info ${this.name}, age ${this.age}, have ${this.money} money`)
+    }
+}
+let zhao = new NewPerson('jerry', 26, 'a lot')
+console.dir(zhao)
+zhao.eat()
+zhao.speak()
+zhao.getInfo()
+// let zhou = new Person('ally', 28)
+// zhou.eat()
+// zhou.speak()
