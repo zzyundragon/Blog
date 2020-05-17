9907ce4918c196668855146347a897c0f734423e
赵云龙 <yunlong.zhao@91wutong.com>
2020-05-17 17:06:49 +0800

    webpack 01

---
+++ webpack/webpack_demo/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js'
+  },
+  mode: 'development'
+}
